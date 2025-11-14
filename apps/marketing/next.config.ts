3be56959fa2763f24545f6897d53f901b5bb6f0a
Daniel James Williams <mail@danielwilliams.dk>
2025-11-14 08:30:04 +0100

    Update next.config.ts

--- apps/marketing/next.config.ts
+++ apps/marketing/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ["nextmq"],
 };
 
 export default nextConfig;
